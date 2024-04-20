9fa9b823ac328bb4b19f01c8d19882080b7d9ab8
Cetecom <Cetecom@SJAPCA03LC0607>
2024-04-20 11:11:17 -0400

    Agregar archivos de proyecto.

---
+++ InversionesLtda.API/Controllers/ProductoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InversionesLtda.API.Models;
+
+namespace InversionesLtda.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private static IList<Producto> lista = new List<Producto>();
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return lista;
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(int id)
+        {
+            return lista.FirstOrDefault(x => x.id == id);
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] Producto value)
+        {
+            lista.Add(value);
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Producto value)
+        {
+            Producto selection = lista.FirstOrDefault(x => x.id == id);
+            lista[lista.IndexOf(selection)] = value;
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lista.Remove(lista.FirstOrDefault(x => x.id == id));
+        }
+    }
+}
---
+++ InversionesLtda.API/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InversionesLtda.API.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+        
+    }
+}
